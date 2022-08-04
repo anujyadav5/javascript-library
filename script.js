26b58c46fc2b4c9644bd20926c0418b849d19335
anujy <anuj.y@hotmail.com>
2022-08-03 20:46:25 -0400

    some html, css, and js (js not working properly yet)

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let myLibrary = [];
+
 function Book(title, author, pages, has_read) {
     this.title = title;
     this.author = author;
@@ -10,3 +12,24 @@ function Book(title, author, pages, has_read) {
         `${title} by ${author}, ${pages} pages, not read yet`;
     }
 }
+
+function addToLibrary(book) {
+    myLibrary.push(book);
+}
+
+function displayBooks(arr) {
+    for (book in arr) {
+        console.log(book);
+        display.innerHTML += `<div class="book"> 
+            <div class="title">${book["title"]}</div>  
+            <div class="author">${book["author"]}</div>  
+            <div class="pages">${book["pages"]}</div>
+            <div class="has-read">${
+                book["has_read"]? "read": "unread"
+            }</div>  
+        </div>`;
+    }
+}
+
+const display = document.getElementsByClassName("display")[0];
+
